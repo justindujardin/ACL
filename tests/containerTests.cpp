1552f842bb21a318550c0964e1e83e51d595a447
Justin DuJardin <insecurity@gmail.com>
2011-06-16 22:37:05 -0700

    Remove test1/2/3 projects and replace with googletest "tests" project. Port example appls to basic tests (all but 1 pass)

---
+++ tests/containerTests.cpp
@@ -0,0 +1,51 @@
+#include "../lib/core/types/types.h"
+#include "../lib/core/containers/tVector.h"
+#include "../lib/core/containers/tList.h"
+#include "../lib/core/containers/tPair.h"
+#include "../lib/core/strings/stringFunctions.h"
+#include<iostream>
+#include <gtest/gtest.h>
+
+
+// Vector insertion and expect values
+TEST(VectorTest,Insertion) {
+   // Vector
+   Vector<S32> intVec;
+   intVec.push_back(1337);
+   intVec.increment();
+   intVec.last() = 12;
+   intVec.push_front(141);
+   EXPECT_TRUE(intVec[0]==141);
+   EXPECT_TRUE(intVec.last()==12);
+   EXPECT_TRUE(intVec[1]==1337);
+}
+
+TEST(ListTest,Insertion) {
+   // List
+   Torque::List<const char *> nameList;
+   static const char *names[] = { "bill", "patricia", "rodney", "colbert", "robert", NULL };
+   S32 i = 0;
+   const char *nameIter = names[i];
+   while(nameIter != NULL)
+   {
+      nameList.pushBack(nameIter);
+      nameIter = names[i++];
+   }
+   i = 0;
+   while(nameList.getSize() > 0)
+   {
+      const char* theName = nameList[0];
+      const char* theOriginal = names[i];
+      EXPECT_STRCASEEQ(theName,theOriginal);
+
+      nameList.popFront();
+      i++;
+   }
+}
+
+TEST(PairTest,Creation) {
+   // Pair
+   Pair<S32,F32> intFloat = MakePair<S32,F32>(1337,1.337f);
+   EXPECT_EQ(intFloat.first,1337);
+   EXPECT_FLOAT_EQ(intFloat.second,1.337f);
+}
---
+++ tests/stringTests.cpp
@@ -0,0 +1,58 @@
+#include "../lib/core/torque.h"
+#include "../lib/core/util/str.h"
+#include "../lib/core/containers/tVector.h"
+#include "../lib/core/strings/stringFunctions.h"
+#include <gtest/gtest.h>
+#include <conio.h>
+
+// Unicode Strings
+#ifdef TORQUE_OS_WIN32
+#include <windows.h>
+TEST(StringTest,UnicodeStrings) {
+   HWND deskWnd = ::GetForegroundWindow();  
+
+   // get text in UTF16 buffer
+   UTF16 unicodeWindowText[256];
+   ::GetWindowTextW(deskWnd,(LPWSTR)&unicodeWindowText,256);
+   String unicodeWindowTextStr = unicodeWindowText;
+
+   UTF8 windowText[256];
+   ::GetWindowTextA(deskWnd,(LPSTR)&windowText,256);
+   String windowTextStr = windowText;
+
+   EXPECT_TRUE(windowTextStr==unicodeWindowTextStr);
+}
+#endif
+
+// Test string constructor
+TEST(StringTest,Construct) {
+   String str1("Hello World");
+   EXPECT_TRUE(str1.c_str() != NULL);
+}
+
+// Test string compares
+TEST(StringTest,Compare) {
+   String str1("Hello World");
+   String str2 = String::ToLower(str1);
+   // Case sensitive
+   EXPECT_FALSE(str1 == str2);
+   EXPECT_TRUE(str1 == str1);
+   // Case insensitive
+   EXPECT_TRUE(str1.equal(str2, String::NoCase));
+}
+
+// Make sure strings work with Vector
+TEST(StringTest,VectorUsage) {
+   // construct/destruct in place
+   Vector<String> strVec;
+   strVec.push_back("test");
+   EXPECT_TRUE(strVec.size() == 1);
+   EXPECT_TRUE(strVec[0] == String("test"));
+
+   // increment insert   
+   String fox = "Big brown box?";
+   strVec.increment();
+   strVec.last() = fox;
+   EXPECT_TRUE(strVec.last() == fox);
+   EXPECT_TRUE(strVec[strVec.size()-1] == fox);
+}
